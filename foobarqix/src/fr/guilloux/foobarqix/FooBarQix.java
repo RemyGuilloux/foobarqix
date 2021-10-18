78dafe525d4d6ac7cbe6f011f2055c3f892e8861
Remy <guillouxrem@gmail.com>
2021-10-18 15:14:28 +0200

    test with BarBar is ok

--- foobarqix/src/fr/guilloux/foobarqix/FooBarQix.java
+++ foobarqix/src/fr/guilloux/foobarqix/FooBarQix.java
@@ -5,6 +5,7 @@ public class FooBarQix {
 		
 		public String given(Integer number) {
 			if (number % 3 == 0) return "FooFoo";
+			if (number % 5 == 0) return "BarBar";
 			return number.toString();
 		}
 	}
--- foobarqix/src/fr/guilloux/foobarqix/FooBarQixTest.java
+++ foobarqix/src/fr/guilloux/foobarqix/FooBarQixTest.java
@@ -8,14 +8,18 @@ import org.junit.Test;
 public class FooBarQixTest {
 
 	@Test
-	public void shoulReturnInputAsString() {
+	public void shouldReturnInputAsString() {
 		Assert.assertEquals("1", new FooBarQix().given(1));
 		Assert.assertEquals("2", new FooBarQix().given(2));
 	}
 	
 	@Test
-	public void shoulReturnFooFooWhenOutisDivisibleBy3() {
+	public void shouldReturnFooFooWhenOutisDivisibleBy3() {
 		Assert.assertEquals("FooFoo", new FooBarQix().given(3));
 	}
 
+	@Test
+	public void shouldReturnBarBarWhenOutisDivisibleBy5() {
+		Assert.assertEquals("BarBar", new FooBarQix().given(5));
+	}
 }
