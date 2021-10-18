566161ab94d3c921754ed2f2e161ac862d2278d3
Remy <guillouxrem@gmail.com>
2021-10-18 23:02:58 +0200

    test divisable complete

--- foobarqix/src/fr/guilloux/foobarqix/FooBarQix.java
+++ foobarqix/src/fr/guilloux/foobarqix/FooBarQix.java
@@ -1,38 +1,20 @@
 package fr.guilloux.foobarqix;
-;
+
 public class FooBarQix {
 	
 		
-		public String imperative(Integer number) {
-			String result ="";
+		public String given(Integer number) {
+			
+			
 			if (number % 3 == 0 && number == 3) return "FooFoo";
 			if (number % 5 == 0 && number == 5) return "BarBar";
 			if (number % 7 == 0 && number == 7) return "QixQix";
-			if (number % 3 == 0) { 
-				  result +="Foo";
-			}
-			if(number % 5 == 0) {
-				result += "Bar";
-			}
-			if(number % 7 == 0) {
-				result += "Qix";
-			}
 			
-			
-			String toFooBar = String.valueOf(number);
-			for (int i = 0; i < toFooBar.length(); i++) {
-				char element = toFooBar.charAt(i);
-				if(element =='3');
-				result += "Foo";
-				if(element =='5');
-				result += "Bar";
-				if(element =='7');
-				result += "Qix";
-			}
-			return result.isEmpty() ? toFooBar : result;
-			
+			if (number % 3 == 0) return "Foo";
+			if (number % 5 == 0) return "Bar";
+			if (number % 7 == 0) return "Qix";
+			return number.toString();
 		}
-		
 	}
 
 
