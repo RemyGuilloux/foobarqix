a8a322917ec2fbccb32989411d56e08f1c759513
Remy <guillouxrem@gmail.com>
2021-10-18 16:02:41 +0200

    element divisible number return FooWhenOutEqual3,BarWhenOutEqual5,QixWhenOutEqual7

--- foobarqix/src/fr/guilloux/foobarqix/FooBarQix.java
+++ foobarqix/src/fr/guilloux/foobarqix/FooBarQix.java
@@ -1,15 +1,38 @@
 package fr.guilloux.foobarqix;
-
+;
 public class FooBarQix {
 	
 		
-		public String given(Integer number) {
+		public String imperative(Integer number) {
+			String result ="";
 			if (number % 3 == 0 && number == 3) return "FooFoo";
 			if (number % 5 == 0 && number == 5) return "BarBar";
 			if (number % 7 == 0 && number == 7) return "QixQix";
-			if (number % 3 == 0) return "Foo";
-			return number.toString();
+			if (number % 3 == 0) { 
+				  result +="Foo";
+			}
+			if(number % 5 == 0) {
+				result += "Bar";
+			}
+			if(number % 7 == 0) {
+				result += "Qix";
+			}
+			
+			
+			String toFooBar = String.valueOf(number);
+			for (int i = 0; i < toFooBar.length(); i++) {
+				char element = toFooBar.charAt(i);
+				if(element =='3');
+				result += "Foo";
+				if(element =='5');
+				result += "Bar";
+				if(element =='7');
+				result += "Qix";
+			}
+			return result.isEmpty() ? toFooBar : result;
+			
 		}
+		
 	}
 
 
--- foobarqix/src/fr/guilloux/foobarqix/FooBarQixTest.java
+++ foobarqix/src/fr/guilloux/foobarqix/FooBarQixTest.java
@@ -9,26 +9,31 @@ public class FooBarQixTest {
 
 	@Test
 	public void shouldReturnInputAsString() {
-		Assert.assertEquals("1", new FooBarQix().given(1));
-		Assert.assertEquals("2", new FooBarQix().given(2));
+		Assert.assertEquals("1", new FooBarQix().imperative(1));
+		Assert.assertEquals("2", new FooBarQix().imperative(2));
 	}
 	
 	@Test
 	public void shouldReturnFooFooWhenOutisDivisibleBy3andEqual3() {
-		Assert.assertEquals("FooFoo", new FooBarQix().given(3));
+		Assert.assertEquals("FooFoo", new FooBarQix().imperative(3));
 	}
 
 	@Test
 	public void shouldReturnBarBarWhenOutisDivisibleBy5andEqual5() {
-		Assert.assertEquals("BarBar", new FooBarQix().given(5));
+		Assert.assertEquals("BarBar", new FooBarQix().imperative(5));
 	}
 	
 	@Test
-	public void shouldReturnBarBarWhenOutisDivisibleBy7andEqual7() {
-		Assert.assertEquals("QixQix", new FooBarQix().given(7));
+	public void shouldReturnQixQixWhenOutisDivisibleBy7andEqual7() {
+		Assert.assertEquals("QixQix", new FooBarQix().imperative(7));
 	}
 	@Test
-	public void shouldReturnBarBarWhenOutisDivisibleBy3() {
-		Assert.assertEquals("Foo", new FooBarQix().given(9));
+	public void shouldReturnFooWhenOutisDivisibleBy3() {
+		Assert.assertEquals("Foo", new FooBarQix().imperative(9));
+	}
+	
+	@Test
+	public void shouldReturnBarWhenOutisDivisibleBy5() {
+		Assert.assertEquals("Bar", new FooBarQix().imperative(10));
 	}
 }
